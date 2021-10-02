b0eec4684ac56f4eaae6440819786791028e125f
ams6293 <amberspatig@mail.weber.edu>
2021-10-02 16:55:29 -0600

    added details pages

--- mysite/home/urls.py
+++ mysite/home/urls.py
@@ -4,7 +4,9 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('hobbies/<int:hobby_id>', views.hobbyDetail, name="hobbyDetail"),
     path('contact/', views.contact, name="contact"),
     path('hobbies/', views.hobbies, name="hobbies"),
-    path('portfolio/', views.portfolio, name="portfolio")
+    path('portfolio/', views.portfolio, name="portfolio"),
+    path('portfolio/<int:portfolio_id>', views.portfolioDetail, name="portfolioDetail")
 ]
--- mysite/home/views.py
+++ mysite/home/views.py
@@ -2,27 +2,48 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Hobbies
 from .models import Portfolios
+from django.template import loader
+
 
 # Create your views here.
 
 
 def home(request):
-    return HttpResponse('My name is Amber Spatig. I\'m a senior here at WSU and my major is computer science. I want to'
-                        'be a front end web developer one day. ')
 
+    return HttpResponse('About Me')
 
 def contact(request):
     return HttpResponse('<b>Amber Spatig:</b> amberspatig@mail.weber.edu')
 
 
 def hobbies(request):
-
-    
     hobby_list = Hobbies.objects.all()
-    return HttpResponse(hobby_list)
+    context = {
+        'hobby_list': hobby_list,
+    }
+    return render(request,  'home/hobbies/hobbyIndex.html', context)
+
+
+def hobbyDetail(request, hobby_id):
+    hobby = Hobbies.objects.get(pk=hobby_id)
+    context = {
+        'hobby': hobby
+    }
+    return render(request, 'home/hobbies/hobbyDetail.html', context)
 
 
 def portfolio(request):
-
     portfolio_list = Portfolios.objects.all()
-    return HttpResponse(portfolio_list)
+
+    context = {
+        'portfolio_list': portfolio_list,
+    }
+    return render(request, 'home/portfolio/portfolioIndex.html', context)
+
+
+def portfolioDetail(request, portfolio_id):
+    portfolio = Portfolios.objects.get(pk=portfolio_id)
+    context = {
+        'portfolio': portfolio
+    }
+    return render(request, 'home/portfolio/portfolioDetail.html', context)
