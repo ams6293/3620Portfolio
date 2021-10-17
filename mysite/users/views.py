f0841448786aad00ec8237579bec461e05bc1a08
ams6293 <amberspatig@mail.weber.edu>
2021-10-17 10:18:26 -0600

    redirects not working, also not saving users

--- mysite/users/views.py
+++ mysite/users/views.py
@@ -1,17 +1,18 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import RegistrationForm
 
 # Create your views here.
 
 
 def register(request):
     if request.method == 'post':
-        form = UserCreationForm(request.POST)
+        form = RegistrationForm(request.POST)
         if form.is_valid():
+            form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Wecome {username}! Your account is created.')
-            return redirect('home/portfolio')
+            messages.success(request, f'Welcome {username}! Your account is created.')
+            return redirect('home:portfolio')
     else:
-        form = UserCreationForm()
+        form = RegistrationForm()
     return render(request, 'users/register.html', {'form': form})
